bb880d342669eacb763aacaba222e5108de03336
Subrata Mohanta <subrata.mohanta@gmail.com>
2025-12-23 12:37:19 +0530

    first commit

---
+++ DotNetCore/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection.AddTransient<IWhetherServiceClient, WhetherForcastClient>();
+serviceCollection.AddTransient<WheatherForcastService>();
+
+using(var service = serviceCollection.BuildServiceProvider())
+{
+    var provider = service.GetRequiredService<WheatherForcastService>();
+    var temprature = provider.GetTemprature("Pune");
+    Console.WriteLine($"The temprature in Pune is {temprature}");
+}
+
+class WheatherForcastService
+{
+    private readonly IWhetherServiceClient _whetherServiceClient;
+
+    public WheatherForcastService(IWhetherServiceClient whetherServiceClient)
+    {
+        _whetherServiceClient = whetherServiceClient;
+    }
+
+    public double GetTemprature(string city)
+    {
+        return _whetherServiceClient.GetCurrentWheatherReport(city).temprature;
+    }
+}
+
+interface IWhetherServiceClient
+{
+    (string city, double temprature) GetCurrentWheatherReport(string city);
+}
+
+class WhetherForcastClient : IWhetherServiceClient
+{
+    public (string city, double temprature) GetCurrentWheatherReport(string city)
+    {
+        // Simulating a call to an external weather service
+        return (city, 32.5);
+    }
+}
---
+++ DotNetFramework/Program.cs
@@ -0,0 +1,56 @@
+﻿using Autofac;
+using System;
+
+namespace DotNetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //build the container
+            var builer = new ContainerBuilder();
+
+            //register types
+            builer.RegisterType<WhetherForcastClient>().As<IWhetherServiceClient>();
+            builer.RegisterType<WheatherForcastService>();
+
+            //resolve dependencies
+            using (var container = builer.Build())
+            {
+                //resolve wheather forcast service
+                var wheatherService = container.Resolve<WheatherForcastService>();
+                var temprature = wheatherService.GetTemprature("Pune");
+                Console.WriteLine($"The temprature in Pune is {temprature}");
+            }
+        }
+    }
+
+    class WheatherForcastService
+    {
+        private readonly IWhetherServiceClient _whetherServiceClient;
+
+        public WheatherForcastService(IWhetherServiceClient whetherServiceClient)
+        {
+            _whetherServiceClient = whetherServiceClient;
+        }
+
+        public double GetTemprature(string city)
+        {
+            return _whetherServiceClient.GetCurrentWheatherReport(city).temprature;
+        }
+    }
+
+    interface IWhetherServiceClient
+    {
+        (string city, double temprature) GetCurrentWheatherReport(string city);
+    }
+
+    class WhetherForcastClient : IWhetherServiceClient
+    {
+        public (string city, double temprature) GetCurrentWheatherReport(string city)
+        {
+            // Simulating a call to an external weather service
+            return (city, 32.5);
+        }
+    }
+}
